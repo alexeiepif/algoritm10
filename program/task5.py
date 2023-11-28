307ba8e39e5a1a85bcea2d33c59d6de4a60bbd45
alexseiepif <aled2093746@gmail.com>
2023-11-28 10:55:31 +0300

    update

--- program/task5.py
+++ program/task5.py
@@ -1,28 +1,58 @@
 import heapq
+from random import randint
 
-def print_sorted_sums(arr1, arr2):
-    if not arr1 or not arr2:
-        return  
 
-    arr1.sort()
-    arr2.sort()
+def print_sorted_sums(list1, list2):
+    if not list1 or not list2:
+        return
 
-    heap = [(arr1[0] + arr2[0], 0, 0)]
+    list1.sort()
+    list2.sort()
+
+    heap = [(list1[0] + list2[0], 0, 0)]
     pushed = {(0, 0)}
+    lenght = []
 
-    for _ in range(len(arr1) * len(arr2)):
+    for _ in range(len(list1) * len(list2)):
+        lenght.append(len(heap))
         sum, i, j = heapq.heappop(heap)
-        print(sum) 
+        print(sum)
+        if (i-1, j-1) in pushed:
+            pushed.discard((i-1, j-1))
+        else:
+            pushed.discard((i, j))
 
-        if i + 1 < len(arr1) and (i + 1, j) not in pushed:
-            heapq.heappush(heap, (arr1[i + 1] + arr2[j], i + 1, j))
+        if i + 1 < len(list1) and (i + 1, j) not in pushed:
+            heapq.heappush(heap, (list1[i + 1] + list2[j], i + 1, j))
             pushed.add((i + 1, j))
 
-        if j + 1 < len(arr2) and (i, j + 1) not in pushed:
-            heapq.heappush(heap, (arr1[i] + arr2[j + 1], i, j + 1))
+        if j + 1 < len(list2) and (i, j + 1) not in pushed:
+            heapq.heappush(heap, (list1[i] + list2[j + 1], i, j + 1))
             pushed.add((i, j + 1))
+    print(lenght)
+    print(f'макс память - {max(lenght)}')
 
-# Пример использования функции:
-arr1 = [4,3,13,2,2,3,45,67,8,2]
-arr2 = [3, 5,34,4,4,5,67,8,6,4]
-print_sorted_sums(arr1, arr2)
+
+def summar(list1, list2):
+    if not list1 or not list2:
+        return
+
+    sum = []
+    for i in list1:
+        for j in list2:
+            sum.append(i+j)
+    sum.sort()
+    # print(sum)
+
+
+def generate_random_list(len_list):
+    random_list = [randint(0, 100
+                           ) for _ in range(len_list)]
+    return random_list
+
+# list1 = [1, 3, 5, 7]
+# list2 = [6, 7, 8, 9]
+list1 = generate_random_list(10)
+list2 = generate_random_list(10)
+print_sorted_sums(list1, list2)
+summar(list1, list2)
